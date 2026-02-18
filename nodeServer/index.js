d71c4a2d77be335af2eb3e8329a8e37f4e948a08
Uday Tharu <160394594+udaytharu@users.noreply.github.com>
2026-02-18 16:05:31 +0000

    update

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -134,8 +134,8 @@ const User = mongoose.model('User', userSchema);
 
 // CORS Configuration
 const allowedOrigins = [
-    'https://chat-app-xir7.onrender.com',
-    'http://chat-app-xir7.onrender.com',
+    'https://chat-app-fog9.onrender.com/',
+    'https://chat-app-fog9.onrender.com/',
     'http://localhost:3000',
     'http://localhost:5500',
     'http://127.0.0.1:5500',
